7b3ea40f8a5268d9ce7e0a8dc268ca8a5a131da4
Siddhesh Mangela <sidthecool007@gmail.com>
2025-09-26 16:49:14 +0530

    fix types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,10 @@ export type {
   InputKeyboardEvent,
 } from '$lib/stories/types.js';
 
-export type { default as getCurrencySymbol } from '$lib/stories/utils/getCurrencySymbol/getCurrencySymbol.js';
+export { default as getCurrencySymbol } from '$lib/stories/utils/getCurrencySymbol/getCurrencySymbol.js';
 
 export { default as NumericInput } from '$lib/stories/NumericInput/NumericInput.svelte';
-export type { NumericInputProps } from '$lib/stories/NumericInput/NumericInput.svelte';
+export type {
+  NumericInputProps,
+  AutoNumericCallbackOptions,
+} from '$lib/stories/NumericInput/NumericInput.svelte';
